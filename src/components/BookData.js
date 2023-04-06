8e9725ad59080c1edb77b8e51314e8448e5ff4c6
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-04-06 12:16:30 +0300

    removing border

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -31,7 +31,7 @@ const AddBook = () => {
         onChange={getInputs}
         name="title"
         value={input.title}
-        placeholder="Title"
+        placeholder=" Book Title"
       />
       <br />
       <input
@@ -39,7 +39,7 @@ const AddBook = () => {
         name="author"
         onChange={getInputs}
         value={input.author}
-        placeholder="Author"
+        placeholder="Categories"
       />
       <br />
       <button type="submit" onClick={handleSubmit}>
